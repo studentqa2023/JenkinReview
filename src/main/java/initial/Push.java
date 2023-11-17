041115b8f0a8778806fd5950fed40c0a35e3ed65
studentqa2023 <rezwanislam857@gmail.com>
2023-11-16 20:55:52 -0500

    first commit

---
+++ src/main/java/initial/Push.java
@@ -0,0 +1,14 @@
+package initial;
+
+public class Push {
+	
+	public void getMethod() {
+		System.out.println("Tuhin");
+		
+	}
+public static void main(String[] args) {
+	Push obj = new Push();
+	obj.getMethod();
+	
+}
+}
